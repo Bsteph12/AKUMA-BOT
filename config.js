9c89f2df327dbcb9d3eb94d11b7e76c96d47b8e5
Bsteph12 <stephaneboyce@gmail.com>
2025-07-30 20:18:02 +0100

    premier commit

---
+++ config.js
@@ -0,0 +1,12 @@
+// config.js
+module.exports = {
+  SESSION_ID: "YERTGTrR#XnGbECG0fL8vIqlx6QgdAfdPG_ro8FfMS_EmyyK2F7o",
+  botName: "Okarun Bot",
+  ownerName: "♱ 𝓓𝓝𝓢 ♱ dev",
+  ownerNumber: "201105950400", // Le numéro du propriétaire
+  prefix: "+",
+  repo: "https://github.com/...", // Le lien de votre dépôt
+  mode: "public" ,// ou "private"
+  newsletterJid: "120363419336864081@newsletter",
+  IMGBB_API_KEY: "254b685aea07ed364f7091dee628d26b"
+};
